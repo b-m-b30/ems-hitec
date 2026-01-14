380aed6888e976111dd99857ad64d48dee3307ca
Matthes Buczko <Matthes@buczko.de>
2026-01-14 11:30:56 +0100

    basic implementation of screen swapping and current Date

--- src/app/ems-home/ems-home.ts
+++ src/app/ems-home/ems-home.ts
@@ -1,17 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal, WritableSignal } from '@angular/core';
+import { EmsQualifications } from "../ems-qualifications/ems-qualifications";
 
 @Component({
   selector: 'app-ems-home',
-  imports: [],
+  imports: [EmsQualifications],
   templateUrl: './ems-home.html',
   styleUrl: './ems-home.css',
 })
 export class EmsHome {
+  selectedScreen: WritableSignal<string> = signal<string>('home');
   @Input({ required: true }) onLogout!: () => void;
   onLogoClick() {}
   onLogoutClick() { this.onLogout(); }
   onProfileClick() {}
   onMitarbeiterClick() {}
-  onQualifikationenClick() {}
+  onQualificationsClick() {this.selectedScreen.set('qualifications');}
   onZuweisenClick() {}
+    get currentYear(): number {
+      return new Date().getFullYear();
+    }
 }
