f665d505913377da30a78bc23a0bd10cc1c0b49f
Matthes Buczko <m.buczko@sportradar.com>
2026-01-20 11:05:06 +0100

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,33 @@
 
+import { authGuard } from './auth/auth-guard';
 import { EmsHome } from './ems-home/ems-home';
 import { LoginComponent } from './login/login';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{ path: '', component: LoginComponent },
-	{ path: 'home', component:  EmsHome },
+	{ path: 'login', component: LoginComponent },
+	{ 
+		path: '',
+		component: EmsHome,
+		canActivate: [authGuard],
+		children: [
+			{
+				path: '',
+				redirectTo: 'home',
+				pathMatch: 'full',
+			},
+			{
+				path: 'home',
+				loadComponent: () => import('./ems-dashboard/ems-dashboard').then(m => m.EmsDashboard),
+			},
+			{
+				path: 'employees',
+				loadComponent: () => import('./ems-employees/ems-employees').then(m => m.EmsEmployees),
+			},
+			{
+				path: 'qualifications',
+				loadComponent: () => import('./ems-qualifications/ems-qualifications').then(m => m.EmsQualifications),
+			}
+		] 
+	},
 ];
