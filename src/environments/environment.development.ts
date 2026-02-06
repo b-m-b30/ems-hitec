74f5f07242c9ce20d7d79b80b0d40e28784349d4
Matthes Buczko <m.buczko@sportradar.com>
2026-02-06 13:46:10 +0100

    fixed environment file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,2 +1,26 @@
-export const environment = {};
-//wird nicht gebraucht
+export const environment = {
+    production: false,
+    api: {
+        employees: 'http://localhost:8089',
+        auth: 'http://localhost:9000',
+    },
+    auth: {
+        tokenUrl: 'http://localhost:9000/application/o/token/',
+        clientId: 'employee_api_client',
+    },
+    polling: {
+        refreshInterval: 30000, // 30 seconds
+        errorTimeout: 5000, // 5 seconds
+    },
+    defaults: {
+        employee: {
+            street: '–',
+            postcode: '00000',
+            phone: '–',
+        },
+    },
+    login: {
+        username: 'john',
+        password: '',
+    },
+};
